416e9394036acb4cfd71f46aa11228b2a315edc6
Ziles131 <54858396+Ziles131@users.noreply.github.com>
2022-01-19 15:00:28 +0200

    implement scss

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,8 @@ import * as React from 'react'
 
 import { Header } from './Header'
 import { Footer } from './Footer'
-import './../styles/reset.css'
-import './../styles/common.css'
-import './../styles/style.css'
+import './../styles/reset.scss'
+import './../styles/common.scss'
 
 export const Layout = ({ isHomepage, children, navigation }) => (
   <>
