9d245f1037ee59456f7a926139c04c64edacfe84
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-01-20 14:29:40 +0200

    add button_connect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import logo from '../../images/header/header_logo.svg'
 // styles
 import * as styles from './header.module.scss'
 
@@ -8,12 +9,31 @@ export const Header = () => {
     <header className={styles.header}>
       <div className={styles.navigation_block}>
         <Link to="/">
-          <span>PembRock</span>
+          <img src={logo} alt='logo'/>
+          {/* <span>PembRock</span> */}
         </Link>
+        <button className={styles.button_connect}>
+          <p className={styles.button_text}>Launch app</p>
+          </button>
+        <div className={styles.header_burger}>
+          <span></span>
+          </div>
         <nav className={styles.navigation}>
-          <ul>
+          <ul className={styles.header_list}> 
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" className={styles.header_link}>Docs</Link>
+            </li>
+            <li>
+              <Link to="/" className={styles.header_link}>Products</Link>
+            </li>
+            <li>
+              <Link to="/" className={styles.header_link}>Roadmap</Link>
+            </li>
+            <li>
+              <Link to="/" className={styles.header_link}>Team</Link>
+            </li>
+            <li>
+              <Link to="/" className={styles.header_link}>Partners</Link>
             </li>
           </ul>
         </nav>
