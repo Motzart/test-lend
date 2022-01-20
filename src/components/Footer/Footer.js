c7e1e44f7f1c1fab13484a8b8b81b5b476a13b65
Ziles131 <54858396+Ziles131@users.noreply.github.com>
2022-01-20 02:26:24 +0200

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// styles
+import * as styles from './footer.module.scss'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.social}></div>
+      <div className={styles.footer_navigation}></div>
+    </footer>
+  )
+}
