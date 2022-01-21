3e0c137dd03923027ffbd02ef43a4f0e68e8c4ae
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-01-21 16:48:25 +0200

    add burger menu css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Link } from 'gatsby'
 import logo from '../../images/header/header_logo.svg'
+import arrows from '../../images/header/arrows.svg'
 // styles
 import * as styles from './header.module.scss'
 
 export const Header = () => {
+  const burgerEl = useRef(null);
+  const menuEl = useRef(null);
+  const buttonEl = useRef(null);
+
   const handleClick = e => {
-    console.log('Hello');
-    // var foo = document.getElementById("burger");
+     const classEl = burgerEl.current.classList ;
+     const classMenuEl = menuEl.current.classList;
+     const classButtonEl = buttonEl.current.classList;
 
-    // for (var i = 0; i < foo.length; i++) {
-    //   foo[i].classList.remove("active");
-    // }
+      classEl.contains(`${styles.active}`) ? 
+      classEl.remove(`${styles.active}`) :
+      classEl.add(`${styles.active}`);
 
-    // e.currentTarget.classList.add("active");
+      classMenuEl.contains(`${styles.active}`) ? 
+      classMenuEl.remove(`${styles.active}`) :
+      classMenuEl.add(`${styles.active}`);
+
+      classButtonEl.contains(`${styles.add_btn}`) ? 
+      classButtonEl.remove(`${styles.add_btn}`) :
+      classButtonEl.add(`${styles.add_btn}`);
+
   }
 
     return (
@@ -23,28 +36,38 @@ export const Header = () => {
         <Link to="/" className={styles.header_logo}>
           <img src={logo} alt='logo'/>
         </Link>
-        {/* <button className={styles.button_connect}>
+        <button className={styles.button_connect} ref={buttonEl}>
           <p className={styles.button_text}>Launch app</p>
-          </button> */}
-        <div className={styles.header_burger} id='burger' onClick={handleClick}>
+          </button>
+        <div className={styles.header_burger} ref={menuEl} onClick={handleClick}>
           <span></span>
         </div>
-        <nav className={styles.header_menu}>
+        <nav className={styles.header_menu} ref={burgerEl}>
           <ul className={styles.header_list}> 
-            <li>
-              <Link to="/" className={styles.header_link}>Docs</Link>
+          <li className={styles.li_a}>
+            <Link to="/" className={styles.header_link_menu}>Menu</Link>
             </li>
-            <li>
+            <li className={styles.li_b}>
+              <Link to="/" className={styles.header_link}>
+                <span className={styles.header_link_text}>Docs</span>
+                </Link>
+              <img src={arrows} alt='arrows' className={styles.arrows}/>
+            </li>
+            <li className={styles.li_b}>
               <Link to="/" className={styles.header_link}>Products</Link>
+              <img src={arrows} alt='arrows' className={styles.arrows}/>
             </li>
-            <li>
+            <li className={styles.li_b}>
               <Link to="/" className={styles.header_link}>Roadmap</Link>
+              <img src={arrows} alt='arrows' className={styles.arrows}/>
             </li>
-            <li>
+            <li className={styles.li_b}>
               <Link to="/" className={styles.header_link}>Team</Link>
+              <img src={arrows} alt='arrows' className={styles.arrows}/>
             </li>
-            <li>
+            <li className={styles.li_b}>
               <Link to="/" className={styles.header_link}>Partners</Link>
+              <img src={arrows} alt='arrows' className={styles.arrows}/>
             </li>
           </ul>
         </nav>
