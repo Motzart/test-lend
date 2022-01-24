271a883a450d4e2af9bd822be45455653a24f7d5
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-01-24 11:01:23 +0200

    del MainContent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 
 import { Header } from './Header/Header'
-import { MainContent } from './MainContent/MainContent'
 import { Footer } from './Footer/Footer'
 import './../styles/reset.scss'
 import './../styles/common.scss'
@@ -10,7 +9,6 @@ export const Layout = ({ isHomepage, children, navigation }) => (
   <>
     <Header isHomepage={isHomepage} navigation={navigation} />
     {children}
-    <MainContent />
     <Footer />
   </>
 )
