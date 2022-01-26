ae4603b222cbecd56fe739f6901afa0cb0c413b4
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-01-26 16:51:06 +0200

    add social block

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,6 +3,7 @@ import React from 'react'
 // styles
 import * as styles from './mainContent.module.scss'
 // components
+import Social from '../Social/Social'
 import Roadmap from './Roadmap/Roadmap'
 
 export const MainContent = () => {
@@ -10,6 +11,7 @@ export const MainContent = () => {
     <main className={styles.container}>
       <section className={styles.information_section}></section>
       <Roadmap />
+      <Social />
       <section className={styles.team_section}></section>
       <section className={styles.community_section}></section>
     </main>
---
+++ src/components/Social/Social.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import twitter from '../../images/social/twitter.svg'
+import elips from '../../images/social/elips.svg'
+import telegram from '../../images/social/telegram.svg'
+import * as styles from './Social.module.scss'
+
+const Social = () => {
+    const iconSocial = [
+        {
+            label: "Telegram",
+            icon: telegram,
+          },
+          {
+            label: "Elips",
+            icon: elips,
+          },
+          {
+            label: "Twitter",
+            icon: twitter,
+          },
+    ]
+
+    return (
+        <nav>
+        <ul className={styles.social_block}> 
+        {iconSocial.map(({icon, label}, idx) => {
+          return(
+             <li key={idx} className={styles.social_element}>
+              <Link to="/">
+                <img src={icon} alt={label} />
+              </Link>
+          </li>
+          )
+            })}
+        </ul>
+      </nav>
+    )
+}
+
+export default Social;
