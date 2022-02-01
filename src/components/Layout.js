8c1e6e7e3852fea59e63c3c7940d4e8307402e4b
Ziles131 <54858396+Ziles131@users.noreply.github.com>
2022-02-01 16:43:53 +0200

    refactor layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,16 @@
+// library
 import * as React from 'react'
-
+// components
 import { Header } from './Header/Header'
 import { Footer } from './Footer/Footer'
+// styles
 import './../styles/reset.scss'
 import './../styles/common.scss'
 
 export const Layout = ({ isHomepage, children, navigation }) => (
-  <>
+  <div className="wrapper">
     <Header isHomepage={isHomepage} navigation={navigation} />
     {children}
     <Footer />
-  </>
+  </div>
 )
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,15 +3,18 @@ import React from 'react'
 // styles
 import * as styles from './mainContent.module.scss'
 // components
+import Info from './Info/Info'
 import Roadmap from './Roadmap/Roadmap'
 
 export const MainContent = ({ pageContent }) => {
   return (
-    <main className={styles.container}>
-      <section className={styles.information_section}></section>
-      <Roadmap data={pageContent} />
-      <section className={styles.team_section}></section>
-      <section className={styles.community_section}></section>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Info />
+        <Roadmap data={pageContent} />
+        <section className={styles.team_section}></section>
+        <section className={styles.community_section}></section>
+      </div>
     </main>
   )
 }
