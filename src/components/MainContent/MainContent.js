3b37aea49057fff5008ec9f0ddc0e531ecf7e107
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-02-01 13:06:03 +0200

    add url target rel. del look

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -4,12 +4,14 @@ import React from 'react'
 import * as styles from './mainContent.module.scss'
 // components
 import Roadmap from './Roadmap/Roadmap'
+import Social from '../Social/Social'
 
 export const MainContent = () => {
   return (
     <main className={styles.container}>
       <section className={styles.information_section}></section>
       <Roadmap />
+      <Social />
       <section className={styles.team_section}></section>
       <section className={styles.community_section}></section>
     </main>
--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -10,24 +10,27 @@ const Social = () => {
         {
             label: "Telegram",
             icon: telegram,
+            url: 'https://t.me/pembrock_finance',
           },
           {
             label: "Elips",
             icon: elips,
+            url: 'https://medium.com/@pembrock.finance',
           },
           {
             label: "Twitter",
             icon: twitter,
+            url: 'https://twitter.com/PembrockFi',
           },
     ]
 
     return (
       <nav>
         <ul className={styles.social_block}> 
-        {iconSocial.map(({icon, label}, idx) => {
+        {iconSocial.map(({icon, label, url}, idx) => {
           return(
              <li key={idx} className={styles.social_element}>
-              <Link to="/">
+              <Link to={url} target='_blank' rel='noreferrer nofollow noopener'>
                <div className={styles.social_circle}>
                   <img src={icon} alt={label} />
                </div>
