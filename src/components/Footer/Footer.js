dfcdb225714a06615f07caa8969c3c2e1bca3426
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-02-02 17:24:52 +0200

    add footer code

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,54 @@
 import React from 'react'
+import Logo from '../Header/Logo/Logo'
+import FooterMenu from './FooterMenu/FooterMenu'
+import Social from '../Social/Social'
+import logoImg from '../../images/header/header_logo.svg'
+import docsArrows from '../../images/header/docs_icon.svg'
 // styles
 import * as styles from './footer.module.scss'
 
 export const Footer = () => {
+  const list = [
+    {
+      label: 'Docs',
+      icon: docsArrows,
+    },
+    {
+      label: 'Products',
+      icon: '',
+    },
+    {
+      label: 'Roadmap',
+      icon: '',
+    },
+    {
+      label: 'Team',
+      icon: '',
+    },
+    {
+      label: 'Partners',
+      icon: '',
+    },
+  ]
   return (
     <footer className={styles.footer}>
-      <div className={styles.social}></div>
-      <div className={styles.footer_navigation}></div>
+      <div className={styles.footer_elements}>
+        <div className={styles.footer_logo}>
+        <Logo img={logoImg} />
+        </div>
+        <div className={styles.footer_navigation}>
+          <FooterMenu list={list} className={styles.footer_menu}/>
+        </div>
+        <p className={styles.social_text}>We are in social networks</p>
+        <div className={styles.social}>
+          <Social className={styles.social}/>
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.copyright_block}>
+        <p className={styles.copyright}>2022 Pembrock Fin Corporation</p>
+        </div>
+      
     </footer>
   )
 }
---
+++ src/components/Footer/FooterMenu/FooterMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'gatsby'
+import * as styles from './FooterMenu.module.scss'
+
+const FooterMenu = ({list}) => {
+    return (
+        <nav className={styles.navigation}>
+        <ul className={styles.header_list}> 
+        {list.map((link, idx) => {
+          return(
+             <li key={idx} className={styles.list_el}>
+              <Link to="/" className={styles.header_link}>
+                {link.label}
+              </Link>
+              {link.icon && <img src={link.icon} className={styles.icon} alt='arrows' />}
+          </li>
+          )
+            })}
+        </ul>
+      </nav>
+    )
+}
+
+export default FooterMenu;
