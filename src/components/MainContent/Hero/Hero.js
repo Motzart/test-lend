ed37007f75d0306a9cc47cecf519dde01f1a735b
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2022-02-02 12:42:29 +0200

    use lower case and add space in scss

--- src/components/MainContent/Hero/Hero.js
+++ src/components/MainContent/Hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
    <div className={styles.block_hero}>
        <div className={styles.hero_text}>
            <h1 className={styles.title}>Leveraged yield farming is 
-               <span className={styles.title_NEAR}>NEAR</span>
+               <span className={styles.title_near}>NEAR</span>
                </h1>
                <p className={styles.text_content}>
                PembRock Finance is the first leveraged yield farming application on NEAR Protocol aimed to provide users with larger yields and NEAR blockchain with greater liquidity. PembRock Finance is almost ready to rock this world! Stay tuned!
