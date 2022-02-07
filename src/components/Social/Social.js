2cb931e54a356d581a690ed1c94c1573774588fe
Ziles131 <54858396+Ziles131@users.noreply.github.com>
2022-02-07 16:04:50 +0200

    replace div to span in social imgs

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React from 'react'
 import twitter from '../../images/social/twitter.svg'
 import elips from '../../images/social/elips.svg'
 import telegram from '../../images/social/telegram.svg'
 import * as styles from './Social.module.scss'
 
 const Social = () => {
-    const iconSocial = [
-        {
-            label: "Telegram",
-            icon: telegram,
-            url: 'https://t.me/pembrock_finance',
-          },
-          {
-            label: "Elips",
-            icon: elips,
-            url: 'https://medium.com/@pembrock.finance',
-          },
-          {
-            label: "Twitter",
-            icon: twitter,
-            url: 'https://twitter.com/PembrockFi',
-          },
-    ]
+  const iconSocial = [
+    {
+      label: 'Telegram',
+      icon: telegram,
+      url: 'https://t.me/pembrock_finance',
+    },
+    {
+      label: 'Elips',
+      icon: elips,
+      url: 'https://medium.com/@pembrock.finance',
+    },
+    {
+      label: 'Twitter',
+      icon: twitter,
+      url: 'https://twitter.com/PembrockFi',
+    },
+  ]
 
-    return (
-      <nav>
-        <ul className={styles.social_block}> 
-        {iconSocial.map(({icon, label, url}, idx) => {
-          return(
-             <li key={idx} className={styles.social_element}>
-              <a href={url} target='_blank' rel='noreferrer nofollow noopener'>
-               <div className={styles.social_circle}>
+  return (
+    <nav>
+      <ul className={styles.social_block}>
+        {iconSocial.map(({ icon, label, url }, idx) => {
+          return (
+            <li key={idx} className={styles.social_element}>
+              <a href={url} target="_blank" rel="noreferrer nofollow noopener">
+                <span className={styles.social_circle}>
                   <img src={icon} alt={label} />
-               </div>
+                </span>
               </a>
-             </li>
+            </li>
           )
-            })}
-        </ul>
-      </nav>
-    )
+        })}
+      </ul>
+    </nav>
+  )
 }
 
-export default Social;
+export default Social
